83cc288b4517f2010bffb64f78e7729ec8c89c90
Justin Young <null@Mac.localdomain>
2025-03-06 09:48:23 -0800

    Pilot easytable to 1.0.2

--- src/main/java/com/sonatype/App.java
+++ src/main/java/com/sonatype/App.java
@@ -1,14 +1,13 @@
 package com.sonatype;
 
-import org.vandeseer.easytable.drawing.cell.ParagraphCellDrawer;
-import org.vandeseer.easytable.structure.cell.paragraph.ParagraphCell;
+import org.vandeseer.easytable.drawing.cell.VerticalTextCellDrawer;
 
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        ParagraphCellDrawer drawer = new ParagraphCellDrawer(ParagraphCell.builder().build());
+        VerticalTextCellDrawer drawer = new VerticalTextCellDrawer();
         System.out.println(drawer.toString());
     }
 }
